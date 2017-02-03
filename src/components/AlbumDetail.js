99a7f983fd8f03b0ed88e6de869c3a99bd569a83
Pierre Ferreira <pierre@tektite.biz>
2017-02-03 20:17:50 +0200

    Update5

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,70 @@
-import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const {
+    title,
+    artist,
+    thumbnail_image,
+    image
+  } = album;
+  const {
+    headerContentStyles,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return (
-    <View>
-      <Text>
-        TITLE OF ALBUM: {props.album.title}
-      </Text>
-    </View>
-  )
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyles}>
+          <Text style={headerTextStyle}> {title}</Text>
+          <Text> {artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyles: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  imageStyle: {
+    width: null,
+    flex: 1,
+    height: 300
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
