036fb350696719724573e2f1e869c793558f2070
Pierre Ferreira <pierre@tektite.biz>
2017-02-03 08:30:26 +0200

    Update4

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+
+const AlbumDetail = (props) => {
+  return (
+    <View>
+      <Text>
+        TITLE OF ALBUM: {props.album.title}
+      </Text>
+    </View>
+  )
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
 
+import AlbumDetail from './AlbumDetail';
+
 class AlbumList extends Component {
-  Constructor() {
-    super(props)
-    let state = {albums: []};
-  }
+  state = { albums: [] }; //Initializing, not modification!
 
   componentWillMount() {
-      console.log('YESSSSSS!!!!');
       axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      // fetch('https://rallycoding.herokuapp.com/api/music_albums')
         .then(response => {
-          console.log('Response:', response);
-          this.setState(albums: response.date)
-        }
+            this.setState({ albums: response.data }); // 'setState' is a method
+                                                      // from Component class.
+                                                      // Always us 'setState'
+                                                      // to modify state! DO NOT
+                                                      // use 'this.state' to
+                                                      // change state.
+          }
         );
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => {
+      return (
+        <AlbumDetail key={album.title} album={album} />
+        // <View key={album.title}>
+        //   <Text>ARTIST: {album.artist}</Text>
+        //   <Text>TITLE: {album.title}</Text>
+        // </View>
+      );
+    });
   }
 
   render() {
     return (
       <View>
-        <Text>Album list!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
